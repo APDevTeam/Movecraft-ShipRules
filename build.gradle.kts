9a1cb562c6ae65d2b3dab4085a3b6a8cb6f76eb5
TylerS1066 <TylerS1066@users.noreply.github.com>
2024-07-06 08:35:56 -0500

    Gradle (#2)
    * Update .gitignore
    
    * Delete pom.xml
    
    * Gradle auto init
    
    * Switch over build system
    
    * Switch over CI

---
+++ build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    `java-library`
+    `maven-publish`
+    id("io.github.0ffz.github-packages") version "1.2.1"
+    id("io.papermc.hangar-publish-plugin") version "0.1.2"
+}
+
+repositories {
+    gradlePluginPortal()
+    mavenLocal()
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven { githubPackage("apdevteam/movecraft")(this) }
+}
+
+dependencies {
+    api("org.jetbrains:annotations-java5:24.1.0")
+    compileOnly("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
+    compileOnly("net.countercraft:movecraft:+")
+}
+
+group = "net.countercraft"
+version = "1.0.0-beta-1_gradle"
+description = "Movecraft-ShipRules"
+java.toolchain.languageVersion = JavaLanguageVersion.of(17)
+
+tasks.jar {
+    archiveBaseName.set("Movecraft-ShipRules")
+    archiveClassifier.set("")
+    archiveVersion.set("")
+}
+
+tasks.processResources {
+    from(rootProject.file("LICENSE.md"))
+    filesMatching("*.yml") {
+        expand(mapOf("projectVersion" to project.version))
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "net.countercraft.movecraft.rules"
+            artifactId = "movecraft-shiprules"
+            version = "${project.version}"
+
+            artifact(tasks.jar)
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/apdevteam/movecraft-shiprules")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
